969e75a8079439f272768ca81d2f8f15e1ccadb9
ali-alzaiat <alialzaiat2000@gmail.com>
2023-12-19 18:02:25 +0200

    created addMessage function

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,10 +1,33 @@
 let Message = require("../models/messages");
 
 let getMessage = (req,res)=>{
-    Message.find({sender:req.body.sender,receiver:req.body.receiver}).populate('receiver').populate('sender')
+    Message.find({sender:req.params.sender,receiver:req.params.receiver}).populate('receiver').populate('sender')
     .then((messages)=>{
         res.status(200).send({messages});
     }).catch((error)=>{
         res.status(500).send(error);
     })
 }
+
+module.exports.getMessage = getMessage;
+
+let addMessage = (req,res)=>{
+    Message.find({sender:req.body.sender,receiver:req.body.receiver}).then((message)=>{
+        if(message){
+            Message.addMessage(req.body.text).then((data)=>{
+                res.status(201).send("message added");
+            });
+        }else{
+            let newMessage = new Message({
+                receiver:req.body.receiver,
+                sender:req.body.sender,
+                content:req.body.text
+            })
+            newMessage.save().then((data)=>{res.status(201).send("message added");});
+        }
+    }).catch((error)=>{
+        res.status(500).send(error);
+    })
+}
+
+module.exports.addMessage = addMessage;
--- models/messages.js
+++ models/messages.js
@@ -2,23 +2,28 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
     sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
     receiver: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
     content: {
-      type: String,
-      required: true
+        type: [String],
+        default: []
     },
     timestamp: {
-      type: Date,
-      default: Date.now
+        type: Date,
+        default: Date.now
     }
   });
+
+messageSchema.methods.addMessage = function(message) {
+    this.content.push(message);
+    return this.save();
+};
   
 module.exports = mongoose.model('Message', messageSchema);
