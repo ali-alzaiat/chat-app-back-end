a7d9eb8cd7368d92181d612948e97123c1659c79
ali-alzaiat <alialzaiat2000@gmail.com>
2023-12-20 20:48:57 +0200

    fixed an error

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,7 +2,7 @@ let jwtUtil = require("../helpers/jwtUtil");
 
 exports.verify = (req,res,next)=>{
     try {
-        const data = jwtUtil.verifyToken(req.body.token);
+        const data = jwtUtil.verifyToken(req.headers.authorization?.split(' ')[1]);
         req.body.data = data;
         next();
     } catch (error) {
