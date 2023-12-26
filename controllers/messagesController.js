de9c136bd8ca84d559c3b17192917ff06b708dce
ali-alzaiat <alialzaiat2000@gmail.com>
2023-12-26 19:53:21 +0200

    fixed error in getMessage function
    send the last message of the chats that the user was part of.

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -18,6 +18,9 @@ module.exports.getMessage = getMessage;
 let getMyMessage = (req,res)=>{
     Message.aggregate([
         {
+          $match: { sender: req.params.sender}
+        },
+        {
           $sort: { timestamp: -1 }
         },
         {
